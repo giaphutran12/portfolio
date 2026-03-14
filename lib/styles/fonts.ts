526c536ba7f957e8a34e574165aef0f8568eb3aa
Edward Tran <giaphutran012@gmail.com>
2026-03-14 22:38:20 +0700

    Revert "perf: fix CLS with section min-heights, explicit font adjustFontFallback"
    This reverts commit 5b6a5569d010d662e3a524200e40fc9af678fb8b.

--- lib/styles/fonts.ts
+++ lib/styles/fonts.ts
@@ -30,7 +30,6 @@ const display = Cormorant_Garamond({
   display: 'swap',
   variable: '--next-font-display',
   preload: true,
-  adjustFontFallback: true,
 })
 
 const body = Syne({
@@ -39,7 +38,6 @@ const body = Syne({
   display: 'swap',
   variable: '--next-font-body',
   preload: false,
-  adjustFontFallback: true,
 })
 
 const fonts = [mono, display, body]
